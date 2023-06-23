c5008d6e877e4c0f4d2fdb855e8dd5d046a13947
Kauarafaldini <kauarsamtos0@gmail.com>
2023-06-23 10:35:26 -0300

    Alteração na conexão da tabela

--- API/app.js
+++ API/app.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-const mensagens = require("./controles/mensagens");
+const clientes = require("./controles/mensagens");
 
-app.use("/mensage", mensagens)
+app.use("/mensage", clientes)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000: http://localhost:3000")
