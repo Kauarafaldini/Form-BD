9b4abeff0d49291f9c0f2fbf2ebbeffaeca22326
Kauarafaldini <kauarsamtos0@gmail.com>
2023-06-26 11:29:24 -0300

    Adição do metodo de envio Cors

--- API/app.js
+++ API/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 
+const cors = require('cors')
+
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    app.use(cors());
+
+    next()
+
+})
 
 const clientes = require("./controles/mensagens");
 
