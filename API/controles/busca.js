629fa132f5c0110a82a6b9dd6424a2c490e7c45a
Kauarafaldini <kauarsamtos0@gmail.com>
2023-06-27 11:25:44 -0300

    Criação do novo metodo de busca

---
+++ API/controles/busca.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const db = require('../db/models/index');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        const { nome, email } = req.query;
+
+        let buscar = {};
+
+        if (nome) {
+            buscar.nome = nome;
+        }
+        if (email) {
+            buscar.email = email;
+        }
+        const clientes = await db.clientes.findALL({ where: buscar});
+
+        return res.json({
+            error: false,
+            mensagem: 'Busca efetuada com sucesso',
+            dados: clientes
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: true,
+            mensagem: 'Erro ao buscar cliente'
+        });
+    }
+});
